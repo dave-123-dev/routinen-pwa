7be1a9491f14fd3391c0b2d7836dd8f308d8b712
dave-123-dev <84165432+dave-123-dev@users.noreply.github.com>
2026-05-28 08:38:44 +0200

    Bump cache for history swipe delete

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='routinen-cache-v8';
+const CACHE='routinen-cache-v9';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
