486ee6d4c6f07d777558b1a7e725af188cd61e8e
dave-123-dev <84165432+dave-123-dev@users.noreply.github.com>
2026-05-28 06:01:05 +0200

    Bump cache version for redesigned UI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-const CACHE='routinen-cache-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+const CACHE='routinen-cache-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
